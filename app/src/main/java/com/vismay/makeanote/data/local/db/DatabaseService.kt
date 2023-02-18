1588eb1eb649b900049e18e94dcd3027c6e009ba
vismay02 <vismay.patil73@gmail.com>
2023-02-19 01:30:39 +0530

    Added Auto migration for Room

--- app/src/main/java/com/vismay/makeanote/data/local/db/DatabaseService.kt
+++ app/src/main/java/com/vismay/makeanote/data/local/db/DatabaseService.kt
@@ -1,17 +1,24 @@
 package com.vismay.makeanote.data.local.db
 
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
+import androidx.room.*
+import androidx.room.migration.AutoMigrationSpec
 import com.vismay.makeanote.data.local.db.dao.NoteDao
 import com.vismay.makeanote.data.local.db.entity.NoteEntity
 import com.vismay.makeanote.data.local.db.typeconverter.DateTypeConverter
 import javax.inject.Singleton
 
 @Singleton
-@Database(entities = [NoteEntity::class], exportSchema = false, version = 1)
+@Database(
+    entities = [NoteEntity::class],
+    version = 3,
+    exportSchema = true,
+    autoMigrations =
+    [AutoMigration(from = 2, to = 3, spec = DatabaseService.ColumDeleteAutoMigration::class)]
+)
 @TypeConverters(DateTypeConverter::class)
-abstract class  DatabaseService : RoomDatabase() {
+abstract class DatabaseService : RoomDatabase() {
+    @DeleteColumn(columnName = "test", tableName = "notes")
+    class ColumDeleteAutoMigration : AutoMigrationSpec
 
     abstract fun NoteDao(): NoteDao
 }
